f41f5484d4273b859fb3794173ba34081ecd8f61
YOGEESH K P <148324344+yogee555@users.noreply.github.com>
2024-04-19 09:42:23 +0530

    Add files via upload

---
+++ if-1.py
@@ -0,0 +1,11 @@
+# If statements
+a=10
+b=20
+if a>b :
+    print("a is greater than b")
+if a<b :
+    print("a is less than b")
+if a==b :
+    print("a is equal to b")
+else:
+    print("a is not greater than b")
